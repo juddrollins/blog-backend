5083cf98c19933212596dd3ea5dd69b520430fb0
Judd Rollins <judd.rollins@gmail.com>
2020-08-03 14:50:38 -0400

    Removed un needed mongo db imports

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-// Mongo imports for db access
-const {MongoClient} = require('mongodb');
-const uri = "mongodb+srv://:@cluster0.bufry.mongodb.net/Cluster0?retryWrites=true&w=majority"
-const client = new MongoClient(uri);
-
 // Setup CORS Policy for app
 app.use(cors());
 // Setup body parsing for app
