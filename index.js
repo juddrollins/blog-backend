471d826bc7124a41edd0730a354ca6fb6f51b4a4
Judd Rollins <judd.rollins@gmail.com>
2020-08-04 15:46:04 -0400

    Added authorization file and imported to index

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+// Custom error handler db interraction errors
 const errorHandler = require("./error/error-handler");
+// JSON web token authentication
+const jwt = require("./authorization/auth");
 
 // Setup CORS Policy for app
 app.use(cors());
 // Setup body parsing for app
 app.use(bodyParser.json());
+// use JWT auth to secure the api
+app.use(jwt());
 
 // Setup handling for user controller file
 require("./controller/user_controller")(app);
